9c7b64e768ddf099d0d6ee333c9cb40af87d7a31
matrixiotbiz <97588037+matrixiotbiz@users.noreply.github.com>
2022-04-24 14:14:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -162,8 +162,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 50) {
-      newMintAmount = 50;
+    if (newMintAmount > 1000) {
+      newMintAmount = 1000;
     }
     setMintAmount(newMintAmount);
   };
